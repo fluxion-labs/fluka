760d8b0a45998ac7dcc8d3b9b30e36e36fd0fc8b
tinguyen <ntrongtin11702@gmail.com>
2025-05-27 05:41:39 +0700

    fix(helix): route config

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -12,12 +12,14 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
-    children: [{ index: true, element: <EVM /> }],
-  },
-  {
-    path: '/helix',
-    element: <Root />,
-    children: [{ index: true, element: <Helix /> }],
+    children: [
+      { index: true, element: <EVM /> },
+      {
+        path: 'helix',
+        element: <Root />,
+        children: [{ index: true, element: <Helix /> }],
+      },
+    ],
   },
 ])
 
